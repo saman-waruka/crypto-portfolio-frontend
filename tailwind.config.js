ca9632d089c7edfec574e299a9f75d8ee221b5a4
saman <saman_waruka@cmu.ac.th>
2024-01-11 15:58:43 +0700

    feat: setup tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "UI-BLACK": "#23252a",
+        "UI-WHITE": "#ffffff",
+        "UI-BLUE": "#5142ff",
+        "UI-DARK-GRAY": "#c3c1d6",
+        "UI-LIGHT-GRAY": "#efefee",
+        "UI-PURPLE": "#7266ff",
+        "UI-DARK-PURPLE": "#6F6AA3",
+        "UI-BORDER": "#dcd9ff29",
+        "UI-SLATE": "#27293a",
+      },
+    },
   },
   plugins: [],
 };
